ea3228d9fe79453900b6acfd4fef4ca6b853782f
carlos <carloseduardolimabastosm@gmail.com>
2021-12-29 18:13:27 -0300

    init

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { HomeContainer } from "./Home.styled";
+import banner from "../../img/banner.jpg";
 
 const Home = () => {
   return (
     <HomeContainer>
-      <h1>Home</h1>
+      <div className="banner">
+        <img src={banner} alt="" />
+      </div>
+      <div className="content">
+        <h1>Welcome to the Home Page</h1>
+      </div>
     </HomeContainer>
   );
 };
